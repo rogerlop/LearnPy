9191c8142a9d7ffc4c0388158904c9646b126ada
Roger L <rogermariolopez@gmail.com>
2022-11-29 08:46:19 -0400

    GET API using requests and json libs

--- FirstAPI.py
+++ FirstAPI.py
@@ -1,10 +1,16 @@
-
 import requests
+import json
 
 URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 response = requests.get(URL)
 
-data = json.loads(response.content.decode("utf-8"))
+#print(response.json())
 
-data
+
+
+data = json.loads(response.content.decode(
+ "utf-8"
+ ))
+
+print(data)
